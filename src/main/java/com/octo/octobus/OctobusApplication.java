88fdf3b3874418828779551523433937e744fc65
ivan.gorobenko <igorobenko@octo.com>
2021-06-04 15:33:40 +0200

    feat: initial commit

---
+++ src/main/java/com/octo/octobus/OctobusApplication.java
@@ -0,0 +1,13 @@
+package com.octo.octobus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctobusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OctobusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/octo/octobus/OctobusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octo.octobus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctobusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
